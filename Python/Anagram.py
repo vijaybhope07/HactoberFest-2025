5e0d84a8f85f95c7e2281de6aa3699aeb6ae214f
Akhzar Farhan <atakhzar@gmail.com>
2024-10-24 23:15:49 +0530

    Update Anagram.py

--- Python/Anagram.py
+++ Python/Anagram.py
@@ -4,8 +4,8 @@ str2=input()
 def anagram(str1, str2): 
  
     if(sorted(str1)== sorted(str2)): 
-        return("The strings_are_anagrams.")  
+        return("The strings are anagrams.")  
     else: 
-        return("The strings_are_not_anagrams.")          
+        return("The strings are not anagrams.")          
  
 print(anagram(str1, str2))
