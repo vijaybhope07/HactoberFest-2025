f96120ddcfc537653868385ad11d55d49d17bac2
Naman-Vasudev <naman24vasudev@gmail.com>
2025-10-21 17:48:31 +0530

    Create anagram_validity.py

---
+++ Python/anagram_validity.py
@@ -0,0 +1,11 @@
+str1=input()
+str2=input()
+
+def anagram(str1, str2): 
+
+    if(sorted(str1)== sorted(str2)): 
+        return("The strings are anagrams.")  
+    else: 
+        return("The strings are not anagrams.")          
+
+print(anagram(str1, str2))
